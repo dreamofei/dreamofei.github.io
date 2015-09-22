5cc74a62667712c4a5369e3bcb5463f4c79c56ea
dreamofei <335328566@qq.com>
2015-09-23 01:47:54 +0800

    完成

--- Libs/Js/ctrls/dateCalcCtrl.js
+++ Libs/Js/ctrls/dateCalcCtrl.js
@@ -1,31 +1,58 @@
 ﻿mainMoudle.controller('dateCalcCtrl', function ($scope, $timeout) {
     var baseObj = new Object();
+
+    //动画相关
+    $scope.LROn = false;//当LROn为true时,ng-class生效，开始d动画
+    $scope.LRIsShow = { visibility: 'hidden' };
+    $scope.CenterOn = false;
+    $scope.CenterIsShow = { visibility: 'hidden' };
+    $scope.openLROn = function () {
+        $scope.LROn = true;
+        $scope.LRIsShow = { visibility: 'visible' };
+        $timeout(function () {
+            $scope.CenterOn = true;
+            $scope.CenterIsShow = { visibility: 'visible' };
+        }, 5000);
+    }
+    //End 动画相关
+
     $scope.calc = function () {
         var beginDateValue = new Date($scope.baseObj.beginDate).valueOf();
         var resultDate = new Date(beginDateValue + $scope.baseObj.addDays * 24 * 60 * 60 * 1000);
         $scope.baseObj.resultDay = resultDate.getFullYear() + "-" + (resultDate.getUTCMonth() + 1) + "-" + resultDate.getDate();
 
-        $scope.leftOn = false;
-        $scope.openLeftOn = function () {
-            $scope.leftOn = true;
-        }
+        //动画相关
         if ($scope.baseObj.resultDay == '2015-9-23') {
-            //setTimeout($scope.$apply($scope.openLeftOn), 1000);
-            //$scope.openLeftOn();
-            $timeout($scope.openLeftOn,3000);
+            $timeout($scope.openOverLay, 10000);
+            //$timeout($scope.openOverLay, 0);
         }
+        //End 动画相关
     }
-
 
 
     //overLay
     //$scope.overLay = { visibility: 'visible' };
     $scope.overLay = { visibility: 'hidden' };
     $scope.openOverLay = function () {
-        $scope.overLay = { visibility: 'hidden' };
+        $scope.overLay = { visibility: 'visible' };
+        $scope.beginRose();
+
+        $timeout(function () {
+            //$scope.overLay = { visibility: 'hidden' };
+            $timeout(function () {
+                $scope.overLay = { opacity: 0.7 };
+                $timeout(function () {
+                    $scope.overLay = { opacity: 0.3 };
+                    $timeout(function () {
+                        $scope.overLay = { visibility: 'hidden' };
+                        $scope.openLROn();
+                    }, 260);
+                }, 260);
+            },260);
+        }, 20000);
     }
 
-    $scope.rose = function(left, height, src) {
+    $scope.rose = function (left, height, src) {
 
         var div = document.createElement("div");
 
@@ -62,11 +89,11 @@
 
             var height = Math.random() * window.innerHeight;
 
-            var src = "../../Sources/imgs/s" + Math.floor(Math.random() * 2 + 1) + ".png";//两张图片分别为"s1.png"、"s2.png"
+            var src = "../../Sources/imgs/R" + Math.floor(Math.random() * 5 + 1) + ".png";//两张图片分别为"s1.png"、"s2.png"
 
             $scope.rose(left, height, src);
 
-        }, 500);
+        }, 300);
     }
 
 
