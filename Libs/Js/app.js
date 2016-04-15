5da47e188620b645cebd822f8b472794f9d05645
dreamofei <335328566@qq.com>
2016-04-15 19:21:29 +0800

    add utils urlCode\base64code

--- Libs/Js/app.js
+++ Libs/Js/app.js
@@ -9,6 +9,12 @@
     .when('/littleUtils/dateCalc', {
         templateUrl: 'Views/LittleUtils/dateCalc.html'
     })
+    .when('/littleUtils/urlCode', {
+        templateUrl: 'Views/LittleUtils/urlCode.html'
+    })
+    .when('/littleUtils/base64Code', {
+        templateUrl: 'Views/LittleUtils/base64Code.html'
+    })
     .otherwise({
         redirectTo: '/'
     });
